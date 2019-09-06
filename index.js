36721c8cbe7fe9d193943dd89bc56500f468b139
francisco-j <ffuentes@ucol.mx>
2019-09-05 20:15:06 -0500

    starting to personalyse project

--- index.js
+++ index.js
@@ -1,43 +1,37 @@
-const path = require("path");
-const express = require("express");
 const mongoose = require("mongoose");
-var bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-
-// connect to  mongoDB //MONGODB_URI defined in heroku
-var mongoUri = (process.env.MONGODB_URI || 'mongodb://localhost/bears-me');
-mongoose.connect(mongoUri,{useNewUrlParser:true})
-.catch((e)=>{
-    console.log(e.message)
-    process.exit(1)
-})
-.then(()=>{
-    console.log("connected to Mongo Atlas")
-})
-
-
-/*
-// configure app to use bodyParser()  // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-*/
-
-
+//require("./routes/web")(app, express);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
+
 app.get("/", (req, res) => {
-  res.render("index", {title: "Home"});
+    res.render("index", { title: "Home" });
+  });
+
+app.get("/user", (req, res) => {
+    res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
 });
 
-app.get("/user", (req, res)=>{
-  res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
-});
-
+/*
+// connect to  mongoDB //MONGODB_URI defined in heroku
+var mongoUri = (process.env.MONGODB_URI || 'mongodb://localhost/bears-me');
+mongoose.connect(mongoUri, { useNewUrlParser: true })
+    .catch((e) => {
+        console.log(e.message)
+        process.exit(1)
+    })
+    .then(() => {
+        console.log("connected to Mongo Atlas")
+    });
+*/
 const port = process.env.PORT || "8000";
 app.listen(port, () => {
-  console.log(`Listening to requests on http://localhost:${port}`);
+    console.log(`Listening to requests on http://localhost:${port}`);
 });
+
