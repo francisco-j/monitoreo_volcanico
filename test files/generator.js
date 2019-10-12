82baf85a55342290674c471a924af3a72904c4f7
francisco-j <ffuentes@ucol.mx>
2019-10-11 19:29:54 -0500

    moved tests to new folder

--- test files/generator.js
+++ test files/generator.js
@@ -1,8 +1,12 @@
-let segundos = 0;
 
+if(process.argv.length<3){
+    console.log(`usage:  generator.js <length>`);
+    return;
+}
+
+let segundos = 0;
 let max = 50;
 let min = 40;
-
 for (let i = 0; i < process.argv[2]; i++) {
     var temperatura=(25+Math.random()*30).toFixed(3);
 
