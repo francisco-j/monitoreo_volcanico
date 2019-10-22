66194e321949eff20b0836a16e0d451aa80f4439
francisco-j <ffuentes@ucol.mx>
2019-10-21 21:56:38 -0500

    random entryes

--- test files/generator.js
+++ test files/generator.js
@@ -1,13 +1,14 @@
 
-if(process.argv.length<3){
-    console.log(`usage:  generator.js <length>`);
-    return;
-}
+if(process.argv.length<3)
+    num = (25+Math.random()*100).toFixed(2);
+else
+    num=process.argv[2];
 
 let segundos = 0;
 let max = 50;
 let min = 40;
-for (let i = 0; i < process.argv[2]; i++) {
+
+for (let i = 0; i < num; i++) {
     var temperatura=(25+Math.random()*15).toFixed(2);
     var ppm=(300+Math.random()*100).toFixed(0);
 
